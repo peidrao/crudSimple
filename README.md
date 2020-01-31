4a8f07a08b798f103957eb6e591cb969089beec6
peidrao <contatopedrorn@gmail.com>
2020-01-31 11:32:26 +0000

    iniciando crud

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ CRUD (Created, Read, Update, Delete)
 
 ## Usando os seguintes frameworks
 * Express: construção de aplicações web para Node.js
+    * Body-Parser (altera o req ou res antes de serem manipulados pelo aplicativo)
 * MongoDB: Banco de dados não relacional.
 * CRUD: POST, GET, PUT, DELETE.
     * POST: Criar (Inserir algum valor no banco de dados)
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 const PORT = 8080;
 
+
+app.use(bodyParser.urlencoded({ extended: true })) // urlencoded informa ao body-parser que é para extarir os dados do elemento do formulário
+
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Olá amigos')
+    res.render('index.ejs')
+})
+
+app.post('/show', (req, res) => {
+    console.log(req.body);
 })
 
 app.listen(PORT, () => {
